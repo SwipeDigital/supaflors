05be18292c59ed45f540ce07e7cf51a9a2d635ff
Fred Bradley <hello@fredbradley.co.uk>
2015-09-04 08:47:16 +0100

    added ing customcss

--- functions.php
+++ functions.php
@@ -1,2 +1,7 @@
 <?php
 show_admin_bar(false);
+function theme_name_scripts() {
+	wp_enqueue_style( 'custom-supaflora', get_stylesheet_directory_uri().'/custom.css' );
+}
+
+add_action( 'wp_enqueue_scripts', 'theme_name_scripts' );
